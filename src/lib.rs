3cdd4342961fd60aab66b7c505ae6e2bf158e127
dracarys18 <karthihegde010@gmail.com>
2022-08-05 14:12:43 +0530

    Remove unneeded include

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-pub mod bindings {
-    #![allow(clippy::all)]
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(unused)]
-    #![allow(improper_ctypes)]
-    include!("./bindings.rs");
-}
+#![allow(clippy::all)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused)]
+#![allow(improper_ctypes)]
+pub mod bindings;
